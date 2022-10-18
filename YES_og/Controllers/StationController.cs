fb3ccdc045e2ba213bd17acd6c7309f4d2bf38e7
Hannie Peng <hannie.peng@yulon-energy.com.tw>
2022-10-18 16:48:46 +0800

    station的新增頁面初版

---
+++ YES_og/Controllers/StationController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YES_og.Models;
+
+namespace YES_og.Controllers
+{
+    public class StationController : _BaseController
+    {
+        private DBePowerDataContext _db = new DBePowerDataContext();
+        // GET: Station
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Station/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Station/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Station/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Station/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Station/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Station/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Station/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
